e27636f431a5daa0cfad6812c04e213387139073
Ivo Kriki <ivo.kriki@outlook.com>
2024-11-04 22:44:30 +0000

    created entities

---
+++ src/main/java/com/ivokriki/spring/security/entities/Tweet.java
@@ -0,0 +1,56 @@
+package com.ivokriki.spring.security.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_tweets")
+public class Tweet {
+
+    @Id
+    @Column(name = "tweet_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long tweetId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private String content;
+
+    @CreationTimestamp
+    private Instant creationTimestamp;
+
+    public Long getTweetId() {
+        return tweetId;
+    }
+
+    public void setTweetId(Long tweetId) {
+        this.tweetId = tweetId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getCreationTimestamp() {
+        return creationTimestamp;
+    }
+
+    public void setCreationTimestamp(Instant creationTimestamp) {
+        this.creationTimestamp = creationTimestamp;
+    }
+}

e4e55f7cfd0536c13a935f1b49fafdf488af9a49
Ivo Kriki <ivo.kriki@outlook.com>
2024-11-04 21:10:53 +0000

    initial commit

---
+++ src/test/java/com/ivokriki/spring/security/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivokriki.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

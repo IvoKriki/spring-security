2af09daf035b1e448ad29d7b7515883229fe662c
Ivo Kriki <ivo.kriki@outlook.com>
2024-11-06 15:23:56 +0000

    changed to the new script to create a table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,7 @@
-INSERT IGNORE INTO tb_roles (role_id, name) VALUES (1, 'admin')
-INSERT IGNORE INTO tb_roles (role_id, name) VALUES (2, 'basic')
+CREATE TABLE IF NOT EXISTS tb_roles (
+    role_id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO tb_roles (role_id, name) VALUES (1, 'admin') ON CONFLICT (role_id) DO NOTHING;
+INSERT INTO tb_roles (role_id, name) VALUES (2, 'basic') ON CONFLICT (role_id) DO NOTHING;

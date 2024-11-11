f35ed58cd26c1e4575ee0b11832f5884e666f99a
Ivo Kriki <ivo.kriki@outlook.com>
2024-11-11 22:13:28 +0000

    made field role optional

--- src/main/java/com/ivokriki/spring/security/repository/RoleRepository.java
+++ src/main/java/com/ivokriki/spring/security/repository/RoleRepository.java
@@ -3,7 +3,9 @@ package com.ivokriki.spring.security.repository;
 import com.ivokriki.spring.security.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }

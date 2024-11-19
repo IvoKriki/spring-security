37ab46047d8750d9e10999304ba00e96356187c3
Ivo Kriki <ivo.kriki@outlook.com>
2024-11-19 22:16:24 +0000

    users controller created

---
+++ src/main/java/com/ivokriki/spring/security/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.ivokriki.spring.security.controller;
+
+import com.ivokriki.spring.security.controller.dto.CreateUserDto;
+import com.ivokriki.spring.security.entities.User;
+import com.ivokriki.spring.security.repository.RoleRepository;
+import com.ivokriki.spring.security.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
+
+@RestController
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserController(UserRepository userRepository,
+                          RoleRepository roleRepository,
+                          BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Transactional
+    @PostMapping("/users")
+    public ResponseEntity<Void> newUser(@RequestBody CreateUserDto userDto){
+
+        var basicRole = roleRepository.findByName("basic");
+        var userFromDb = userRepository.findByUsername(userDto.password());
+        if (userFromDb.isPresent())
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+
+        var user = new User();
+        user.setUsername(userDto.username());
+        user.setPassword(userDto.password());
+        user.setPassword(bCryptPasswordEncoder.encode(Set.of(basicRole).toString()));
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok().build();
+    }
+
+}

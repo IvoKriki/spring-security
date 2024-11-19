e536bde163890a4b42b6735b4965891259cdbb57
Ivo Kriki <ivo.kriki@outlook.com>
2024-11-19 22:15:48 +0000

    new route added

--- src/main/java/com/ivokriki/spring/security/config/SecurityConfig.java
+++ src/main/java/com/ivokriki/spring/security/config/SecurityConfig.java
@@ -38,8 +38,8 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/login")
-                        .permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
                         .anyRequest()
                         .authenticated())
                 .csrf(csrf -> csrf.disable())
